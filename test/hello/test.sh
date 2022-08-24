3feb1ddeb83f824037e32c463ca5b836e3e4d973
yunnysunny <yunnysunny@gmail.com>
2022-08-24 14:04:46 +0800

    ci: remove container test script

--- test/hello/test.sh
+++ test/hello/test.sh
@@ -10,12 +10,12 @@ MY_NAME=myhello
 docker build . -t ${MY_NAME} --build-arg APP_ID=${APP_ID} \
 --build-arg APP_SECRET=${APP_SECRET} \
 --build-arg IMAGE_VERSION=${IMAGE_VERSION}
-# 测试脚本
+# test script
 container_id=$(docker run -e APP_PORT="${APP_PORT}" -p ${APP_PORT}:${APP_PORT}  -d ${MY_NAME})
-docker exec -i "${container_id}" node -e "console.log(new Date().toString())"
-docker exec -i "${container_id}" ps -ef | grep agent
-sleep 3
-curl http://localhost:${APP_PORT}
-docker stop "${container_id}"
-docker rm "${container_id}"
+# docker exec -i "${container_id}" node -e "console.log(new Date().toString())"
+# docker exec -i "${container_id}" ps -ef | grep agent
+# sleep 3
+# curl http://localhost:${APP_PORT}
+# docker stop "${container_id}"
+# docker rm "${container_id}"
 
